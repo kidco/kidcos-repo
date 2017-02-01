e13a93173ed0a0207368f50cabaaeae45ae7a590
ryang999 <ryang999@LHS-LAB608-32.lhs.win.fuhsd.org>
2017-02-01 14:26:44 -0800

    oh loard what im i doing lul

---
+++ Sudoku Solver/Possibilities.java
@@ -0,0 +1,81 @@
+import java.util.ArrayList;
+
+
+public class Possibilities
+{
+    private ArrayList<Integer> poss;
+
+
+    public Possibilities()
+    {
+        poss = new ArrayList<Integer>( 9 );
+        for ( int i = 1; i <= 9; i++ )
+        {
+            poss.add( i );
+        }
+    }
+
+
+    /**
+     * Returns the value if there is only one remaining value.
+     * 
+     * @return the one remaining value
+     */
+    public int getValue()
+    {
+        if ( poss.size() == 1 )
+        {
+            return poss.get( 0 );
+        }
+        return 0;
+       
+    }
+
+
+    public boolean isEmpty()
+    {
+        if ( poss.size() == 0 )
+        {
+            return true;
+        }
+        return false;
+    }
+
+
+    /**
+     * removes x from possibilities if x exists in there
+     * 
+     * @param x
+     *            number to be removed
+     */
+    public void remove( int x )
+    {
+        if ( poss.contains( x ) )
+        {
+            poss.remove( new Integer( x ) );
+        }
+    }
+
+
+    public void clear()
+    {
+        poss.clear();
+    }
+
+
+    public void printPossibilities()
+    {
+        for ( int i = 0; i < poss.size(); i++ )
+        {
+            System.out.print( poss.get( i ) + " " );
+        }
+    }
+
+
+    public static void main( String[] args )
+    {
+        Possibilities joe = new Possibilities();
+        joe.remove( 3 );
+        joe.printPossibilities();
+    }
+}
---
+++ Sudoku Solver/Solve.java
@@ -0,0 +1,10 @@
+
+public class Solve
+{
+    public static void main( String[] args )
+    {
+        Board zzz = new Board();
+        zzz.printBoard();
+        zzz.solveBoard();
+    }
+}
