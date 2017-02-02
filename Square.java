68efa7d524ad71a0904b8652130c20bfa01290da
Winston <Winston@192.168.1.126>
2017-02-01 16:51:36 -0800

    Plzpzlpplzasdgseg

--- Square.java
+++ Square.java
@@ -1,4 +1,16 @@
 
+/**
+ *  TODO Write a one-sentence summary of your class here.
+ *  TODO Follow it with additional details about its purpose, what abstraction
+ *  it represents, and how to use it.
+ *
+ *  @author  Winston
+ *  @version Feb 1, 2017
+ *  @author  Period: TODO
+ *  @author  Assignment: Sudoku Solver
+ *
+ *  @author  Sources: TODO
+ */
 public class Square
 {
     private Possibilities poss;
