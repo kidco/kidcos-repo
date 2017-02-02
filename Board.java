73741d0767d345040f6d44eee2ea592ed1a2c58e
Winston <Winston@192.168.1.126>
2017-02-01 16:47:44 -0800

    Testing #2

--- Board.java
+++ Board.java
@@ -2,16 +2,12 @@ import java.util.Scanner;
 
 
 /**
- *  TODO Write a one-sentence summary of your class here.
- *  TODO Follow it with additional details about its purpose, what abstraction
- *  it represents, and how to use it.
+ *  Model and solver of Sudoku board.
  *
- *  @author  Winston
+ *  @author  Winston Tsai, Richard Yang
  *  @version Feb 1, 2017
- *  @author  Period: TODO
  *  @author  Assignment: Sudoku Solver
  *
- *  @author  Sources: TODO
  */
 public class Board
 {
